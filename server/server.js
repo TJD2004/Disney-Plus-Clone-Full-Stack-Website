7c5dd89e95944e6d48417c55578ba4fd9971503e
unknown <dharmiktushar2004@gmail.com>
2025-08-09 18:58:07 +0530

    change in folder structure

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173','https://disney-plus-clone-full-stack-websit.vercel.app/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 app.use(express.json());
