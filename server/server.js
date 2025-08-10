440677feac85a9fcf1b89b9142a7292c93b17fb9
unknown <dharmiktushar2004@gmail.com>
2025-08-10 08:42:04 +0530

    Changes in some files

--- server/server.js
+++ server/server.js
@@ -14,10 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173','https://disney-plus-clone-full-stack-websit.vercel.app/'],
+  origin: ['http://localhost:5173','https://disney-plus-clone-full-stack-websit.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
